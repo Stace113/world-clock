9162ebf32990f0bfe1018bb9b66f3d5b5c03723f
Stace113 <stace-w-13@live.co.uk>
2024-07-30 10:46:27 +0100

    added js time and date with moment function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+let bristolElement = document.querySelector("#bristol");
+let bristolDateElement = bristolElement.querySelector(".date");
+let bristolTimeElement = bristolElement.querySelector(".time");
 
+bristolDateElement.innerHTML = moment().format("dddd");
